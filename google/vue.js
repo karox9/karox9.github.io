3f7bd167d6bd4caa2071078fe2b82e48b87a4eb2
karox9 <karolina.jakobik1999@gmail.com>
2021-04-13 22:04:04 +0200

    zajęcia6

--- google/vue.js
+++ google/vue.js
@@ -1,6 +1,20 @@
 var app = new Vue({
     el: '#app',
     data: {
-       googleSearch: '',
+       googleSearch: "",
+       cities: window.cities,
+    },
+
+    computed: {
+       filteredCities() {
+          return this.cities
+          
+          .filter((city) => {
+             return city.name.toLowerCase().includes(this.googleSearch);
+          })
+
+          .slice(0,10);
+       }
     }
+
   });
