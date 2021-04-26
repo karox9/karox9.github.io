4dbf38090466b473ad167221b0a98ec84842c8c5
karox9 <karolina.jakobik1999@gmail.com>
2021-04-26 21:57:25 +0200

    zajecia7

--- google/vue.js
+++ google/vue.js
@@ -3,6 +3,8 @@ var app = new Vue({
     data: {
        googleSearch: "",
        cities: window.cities,
+       text: "",
+       value: 0,
     },
 
     computed: {
@@ -13,8 +15,25 @@ var app = new Vue({
              return city.name.toLowerCase().includes(this.googleSearch);
           })
 
-          .slice(0,10);
+          .slice(0, 10);
        }
-    }
+    },
 
+    methods: {
+      enter() {
+          this.value = 0;
+          this.text = this.googleSearch;
+      },
+
+      fill(city) {
+          this.googleSearch = city.name;
+          this.text = this.googleSearch;
+          this.value = 0;
+      },
+
+      list() {
+         this.value = 1;
+     }
+  }
   });
+
