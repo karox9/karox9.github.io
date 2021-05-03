818acc7481248b956971e264d493297c03cc850b
karox9 <karolina.jakobik1999@gmail.com>
2021-05-03 22:19:44 +0200

    zaj8

--- google/vue.js
+++ google/vue.js
@@ -1,39 +1,107 @@
 var app = new Vue({
     el: '#app',
     data: {
-       googleSearch: "",
-       cities: window.cities,
-       text: "",
-       value: 0,
+      googleSearch: "",
+      cities: window.cities,
+      searchedInput:'',
+      filteredCities:"",
+      filteredCities2: true,
+      el_focus: false,
+      change: false,
+      mark: -1,
     },
 
-    computed: {
-       filteredCities() {
-          return this.cities
-          
-          .filter((city) => {
-             return city.name.toLowerCase().includes(this.googleSearch);
-          })
+    watch: {
+        mark() {
+            this.filteredCities2 = false;
+            this.googleSearch = this.filteredCities[this.mark].name;
+        },
 
-          .slice(0, 10);
-       }
+        googleSearch() {
+            this.create(this.filteredCities2);
+            this.filteredCities2 = true;
+            console.log(this.filteredCities); 
+
+            if (this.mark == -1)
+            {
+                this.searchedInput = this.googleSearch;      
+            }
+        }
     },
 
     methods: {
-      enter() {
-          this.value = 0;
-          this.text = this.googleSearch;
-      },
+        down(){
+            if (this.mark < this.filteredCities.length-1)
+            {
+                this.mark += 1; 
+            }
+            else if (this.mark == this.filteredCities.length-1)
+            {
+                this.mark = 0; 
+            }
+        },
+        
+        up() {
+            if (this.mark > 0)
+            {
+                this.mark -= 1; 
+            }
+                else if (this.mark == 0)
+                {
+                    this.mark = this.filteredCities.length-1;
+                }
+        },
 
-      fill(city) {
-          this.googleSearch = city.name;
-          this.text = this.googleSearch;
-          this.value = 0;
-      },
+        enter() {
+            this.filteredCities = true;
+            this.change = true;
+            this.mark = -1;
+            this.el_focus = false;
+        },
 
-      list() {
-         this.value = 1;
-     }
-  }
+        chosen(i) {
+            this.googleSearch = this.filteredCities[i].name;
+            this.enter();
+        },
+             
+        active() {
+            if (this.googleSearch.length == 0)
+            {
+                this.change = false;
+            }
+            
+            return this.change;
+        },
+            
+        create(value) {
+            if (value)
+            {
+                let result = this.cities.filter(city => city.name.includes(this.googleSearch));
+                
+                if (result.length > 10)
+                {
+                    this.filteredCities = result.slice(0, 10);
+                }
+
+                else 
+                {
+                    this.filteredCities = result;
+                }
+
+                this.mark = -1;
+            }   
+        },
+             
+        bold_letter(city) {
+            let re = new RegExp(this.searchedInput, "gi");
+            let bolden = "<b>" + city.name.replace(re, match =>
+                {
+                    return "<span class='without_bold'>" +  match + "</span>";
+                }) + "</b>";
+
+            console.log(bolden);
+            return bolden;
+        }      
+    }
   });
 
